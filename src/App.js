0107d1b6188c6d5082cd96d7a057e1dcd67552e2
decobang <davidcoggin123@gmail.com>
2024-01-30 01:25:51 -0600

    added 3 containers, for input field, todo and completed container

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ function App() {
   return (
     <div className={styles['App']}>
       <h1>Do It All</h1>
+      <div className={styles['main-container']}>
+        <p>This is the Iput Field</p>
+        <div className={styles['todos-container']}>
+         <div className={styles['todo-container']}>Todo Container</div>
+          <div className={styles['completed-container']}>Completed Container</div>
+        </div>
+        </div>
     </div>
   );
 }
